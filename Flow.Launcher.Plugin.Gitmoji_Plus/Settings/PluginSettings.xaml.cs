8ef2a7499e2a66dad77101d219f332f80b738a51
tho-myr <thomas.myr.dev@gmail.com>
2025-03-12 11:23:57 +0100

    ✨ add setting to choose between emoji (✨) or code (:sparkles:) as property to copy on click/enter
    #4

---
+++ Flow.Launcher.Plugin.Gitmoji_Plus/Settings/PluginSettings.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Controls;
+
+namespace Flow.Launcher.Plugin.Gitmoji_Plus.Settings;
+
+public partial class PluginSettings : UserControl
+{
+    
+    private readonly PluginInitContext _context;
+    private readonly Settings _settings;
+
+    public PluginSettings(PluginInitContext context, Settings settings)
+    {
+        InitializeComponent();
+        _context = context;
+        _settings = settings;
+        LoadSettings();
+    }
+
+    private void LoadSettings()
+    {
+        CopyActionComboBox.SelectedIndex = _settings.PropertyToCopy switch
+        {
+            Settings.CopyAction.Emoji => 0,
+            Settings.CopyAction.Code => 1,
+            _ => 0
+        };
+    }
+    
+    private void CopyActionComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        SaveSettings();
+    }
+
+    private void SaveSettings()
+    {
+        _settings.PropertyToCopy = CopyActionComboBox.SelectedIndex switch
+        {
+            0 => Settings.CopyAction.Emoji,
+            1 => Settings.CopyAction.Code,
+            _ => Settings.CopyAction.Emoji
+        };
+        
+        _context.API.SaveSettingJsonStorage<Settings>();
+    }
+}
---
+++ Flow.Launcher.Plugin.Gitmoji_Plus/Settings/Settings.cs
@@ -0,0 +1,11 @@
+﻿namespace Flow.Launcher.Plugin.Gitmoji_Plus.Settings;
+
+public class Settings {
+
+    public enum CopyAction {
+        Emoji, Code
+    }
+    
+    public CopyAction PropertyToCopy { get; set; }
+    
+}
